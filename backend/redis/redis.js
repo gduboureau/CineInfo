0b4c716aeaab7d4a5ff7b3989d50c1ebb85567ab
Jennifer <ephrem.jennifer@gmail.com>
2023-12-23 12:56:47 +0100

    config redis et bdd ok

---
+++ backend/redis/redis.js
@@ -0,0 +1,55 @@
+// redis.js
+import { createClient } from 'redis';
+
+const client = createClient({ url: 'redis://redis:6379' });
+
+async function connectToRedis() {
+    if (client.isOpen) {
+        console.log('Redis client already connected');
+        return;
+    }
+    else {
+        console.log('Connecting to Redis...');
+        await client.connect();
+    }
+}
+
+async function getFromCache(key) {
+    console.log(`Getting ${key} from Redis`);
+    return await client.get(key, (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            return JSON.parse(data);
+        }
+    });
+}
+
+async function saveToCache(key, data) {
+    console.log(`Saving ${key} to Redis`);
+    await client.set(key, JSON.stringify(data), (err, reply) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(reply);
+        }
+    });
+}
+
+async function removeFromCache(key) {
+    console.log(`Removing ${key} from Redis`);
+    await client.del(key, (err, reply) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(reply);
+        }
+    });
+}
+
+function closeConnection() {
+    console.log('Closing Redis connection');
+    client.quit();
+}
+
+export { connectToRedis, getFromCache, saveToCache, removeFromCache, closeConnection };
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,53 @@
 import express from 'express';
-import db from './utils/pg.js';
+import fetch from 'node-fetch';
+import { connectToRedis, getFromCache, saveToCache, removeFromCache, closeConnection } from './redis/redis.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', async (req, res) => {
-  try {
+  // Exemple connection postgres
+
+  /*try {
     const result = await db.query('SELECT * FROM public."Users"');
-    res.send(result.rows[0]); 
+    res.send(result.rows); 
   } catch (error) {
     console.error('Erreur lors de la requête:', error);
     res.status(500).send('Erreur lors de la requête'); 
-  }
-});
+  }*/
 
+
+  // Exemple connection redis 
+
+  
+  /*try {
+    await connectToRedis();
+
+    const apiKey = '7f0799a761376830477332b8577e17fe';
+    const tmdbResponse = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+    const tmdbData = await tmdbResponse.json();
+
+    const movies = tmdbData.results;
+
+    // Stocker les données dans Redis
+    for (const movie of movies) {
+      const key = `movie:${movie.id}`;
+      await saveToCache(key, movie);
+      console.log(`Movie with ID ${movie.id} stored in Redis`);
+    }
+
+    // Exemple de récupération d'un film spécifique
+    const movieId = 940721; // Remplacez par l'ID d'un film réel
+    const storedMovie = await getFromCache(`movie:${movieId}`);
+    console.log(`Retrieved movie from Redis: ${JSON.stringify(storedMovie)}`);
+
+  } catch (error) {
+    console.error('Erreur lors de la connexion à Redis ou de la récupération des données TMDB:', error);
+  } finally {
+    closeConnection();
+  }*/
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
