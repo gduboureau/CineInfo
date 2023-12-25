a100da9dd7e9cc63c2abd86975fc34b5ffed9fa6
Jennifer <ephrem.jennifer@gmail.com>
2023-12-25 18:13:13 +0100

    add routes display movies/series

--- frontend/src/components/movies/DisplayMovies.jsx
+++ frontend/src/components/movies/DisplayMovies.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import moment from 'moment';
 import 'moment/locale/fr';
 
 import "./assets/displayMovies.css";
 
 const DisplayMovies = ({ movies }) => {
+    const navigate = useNavigate();
+
+    const handleClick = (id, title) => {
+        navigate(`/movie/${id}/${title}`);
+    }
 
     return (
         <div className="movies-container">
             {movies.map((movie) => (
                 <div key={movie.id} className="movie">
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                    <p>{movie.title}</p>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} onClick={() => handleClick(movie.id, movie.title)}/>
+                    <p className="title" onClick={() => handleClick(movie.id, movie.title)}>{movie.title}</p>
                     <p>{moment(movie.release_date).locale('fr').format('D MMM YYYY')}</p>
                 </div>
             ))}
