59a8d58a149381e344c3bd3799ed8382d1940a70
val-lst <vloustau3@gmail.com>
2023-12-26 20:46:59 +0100

    main page v1

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -1,12 +1,59 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-import Carousel from './CarouselPopmovies';
+import MainCarousel from './MainCarousel';
+import CarouselByCategory from './CarouselByCategory';
+import './assets/Homepage.css';
 
 const Homepage = () => {
+
+  const [mainMovies, setMainMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+
+  useEffect(() => {
+      fetch("http://localhost:8080/movies/popular", {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+          },
+      })
+          .then((response) => response.json())
+          .then((data) => setMainMovies(data))
+          .catch((error) => console.error("Erreur de recherche :", error));
+
+        fetch("http://localhost:8080/movies/now-playing", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => setNowPlayingMovies(data))
+            .catch((error) => console.error("Erreur de recherche :", error));
+
+            fetch("http://localhost:8080/movies/top-rated", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => setTopRatedMovies(data))
+                .catch((error) => console.error("Erreur de recherche :", error));
+  }, []);
+
     return (
-        <div>
+        <div style={{backgroundColor: '#0c0c0c'}} className="home">
             <div className="home-carroussel">
-                <Carousel />
+                <MainCarousel movies={mainMovies}/>
+            </div>
+            <div className="home-nowplayingmovies">
+                <h3>Actuellement au cinéma</h3>
+                <CarouselByCategory movies={nowPlayingMovies}/>
+            </div>
+            <div className="home-toprated">
+                <h3>Les mieux notés</h3>
+                <CarouselByCategory movies={topRatedMovies}/>
             </div>
         </div>
     )
---
+++ frontend/src/pages/homepage/MainCarousel.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './assets/MainCarousel.css';
+
+const MainCarousel  = ({ movies }) => {
+  function Arrow({ direction, onClick }) {
+    return (
+      <div
+        className={`arrow ${direction}`}
+        onClick={onClick}
+      >
+        {direction === 'left' ? (
+          <FontAwesomeIcon icon={faChevronLeft} />
+        ) : (
+          <FontAwesomeIcon icon={faChevronRight} />
+        )}
+      </div>
+    );
+  }
+
+  var settings = {
+    // dots: true,
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "29%",
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <Arrow direction="right" />,
+    prevArrow: <Arrow direction="left" />,
+    autoplay: true,
+    autoplaySpeed: 7000
+  };
+
+
+
+  return (
+    <div className="carousel-popmovies-container">
+      <div className="carousel-popmovies">
+        <Slider {...settings}>
+            {movies.slice(0, 10).map((movie, index) => (
+              <div key={index} className={`carousel-item`}>
+                <img src={`https://image.tmdb.org/t/p/w1920_and_h1080_multi_faces${movie.backdrop_path}`} alt={`Slide ${index + 1}`} />
+                  <p className='title'>{movie.title}</p>
+                  <p className='dureenote'>1h50 | {movie.vote_average.toFixed(1)}/10</p>
+                  <p className='overview'>{movie.overview.length > 500 ? 
+                      movie.overview.substring(0, 500) + '...' : movie.overview}
+                  </p>
+              </div>
+            ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default MainCarousel;
